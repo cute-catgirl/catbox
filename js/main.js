22f018a3dc292562df014c2408a9d40681745230
Ben <66697315+Boxit379@users.noreply.github.com>
2021-05-17 13:04:47 -0600

    RNG
    Random Number Generator or Really Not Good?

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var catNum = getRandomInt(1, 3);
+console.log(catNum);
+
+function openBox() {
+
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
