f0dca99206d6f2dbdd430427ef617a54c0a99b35
Ben <66697315+Boxit379@users.noreply.github.com>
2021-05-17 20:26:39 -0600

    It works now.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,32 @@
 var catNum = 0;
+var creditsList = ['Photo by <a href="https://unsplash.com/@e_d_g_a_r?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Edgar</a> on <a href="https://unsplash.com/s/photos/cat?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>','Photo by <a href="https://unsplash.com/@remino?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Rémi Rémino</a> on <a href="https://unsplash.com/s/photos/cat?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>','Photo by <a href="https://unsplash.com/@michaelsum1228?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Michael Sum</a> on <a href="https://unsplash.com/s/photos/cat?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>','Photo by <a href="https://unsplash.com/@mimvafa?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Marzie Vafa</a> on <a href="https://unsplash.com/s/photos/cat?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>'];
+
+$(function () {
+  animateFloat();
+});
+
 
 $("#Cat").click(function(){
   catNum = getRandomInt(1, 4);
-  $(this).src = "images/cat" + catNum + ".png";
-  console.log("cat")
+  if ($(this).attr('src') == 'images/box.png') {
+    $(this).attr("src","images/cat" + catNum + ".png");
+    $("#Credits").html(creditsList[catNum-1]);
+  }
 });
+
+function animateFloat() {
+  $("#Container").animate({height: '110vh'},1000);
+  $("#Container").animate({height: '90vh'},1000,animateBack);
+}
+
+function animateBack() {
+  if ($('#Cat').attr('src') == 'images/box.png') {
+    animateFloat();
+  }
+  else {
+    $("#Container").animate({height: '100vh'},500);
+  }
+}
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
