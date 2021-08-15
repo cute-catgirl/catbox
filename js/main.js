a9b676cd38722035926dc6cc460b8fda7c706923
Ben <benpmoon@gmail.com>
2021-08-15 16:24:20 -0600

    events

--- js/main.js
+++ js/main.js
@@ -27,8 +27,7 @@ $("#Cat").click(function(){
     boxesOpened++;
     localStorage.setItem("boxSave", JSON.stringify(boxesOpened));
     document.getElementById("amountOpened").innerHTML = boxesOpened;
-    
-    
+    dataLayer.push({'event': 'box-opened'});
     conffetiBlast();
     $(this).attr("src","images/cat" + catNum + ".png");
     if (catsFound.includes("images/cat" + catNum + ".png") == false) {
