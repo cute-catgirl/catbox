96f68de0ad29f3da427d0fdbec99ee989d9f1e7d
Ben <benpmoon@gmail.com>
2021-08-15 15:43:45 -0600

    added google analytics and boxes opened counter

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 var catNum = 0;
 var creditsList = ['Photo by <a href="https://unsplash.com/@e_d_g_a_r?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Edgar</a> on <a href="https://unsplash.com/s/photos/cat?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>','Photo by <a href="https://unsplash.com/@remino?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Rémi Rémino</a> on <a href="https://unsplash.com/s/photos/cat?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>','Photo by <a href="https://unsplash.com/@michaelsum1228?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Michael Sum</a> on <a href="https://unsplash.com/s/photos/cat?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>','Photo by <a href="https://unsplash.com/@mimvafa?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Marzie Vafa</a> on <a href="https://unsplash.com/s/photos/cat?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>','Photo by <a href="https://unsplash.com/@milada_vigerova?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Milada Vigerova</a> on <a href="https://unsplash.com/s/photos/cat?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>'];
 var catsFound = [];
+var boxesOpened = 0;
 if (localStorage.getItem("catsSave") != null) {
   catsFound = JSON.parse(localStorage.getItem("catsSave"));
 }
+if (localStorage.getItem("boxSave") != null) {
+  boxesOpened = JSON.parse(localStorage.getItem("boxSave"));
+}
 document.getElementById("amountFound").innerHTML = catsFound.length;
+document.getElementById("amountOpened").innerHTML = boxesOpened;
 
 $(function () {
   animateFloat();
@@ -19,6 +24,9 @@ $.modal.defaults = {
 $("#Cat").click(function(){
   catNum = getRandomInt(1, 5);
   if ($(this).attr('src') == 'images/box.png') {
+    boxesOpened++;
+    localStorage.setItem("boxSave", JSON.stringify(boxesOpened));
+    document.getElementById("amountOpened").innerHTML = boxesOpened;
     conffetiBlast();
     $(this).attr("src","images/cat" + catNum + ".png");
     if (catsFound.includes("images/cat" + catNum + ".png") == false) {
