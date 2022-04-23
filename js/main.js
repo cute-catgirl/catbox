3fe2a60d26938de141ff36298c20f53b0ae6fad7
Ben <66697315+Boxit379@users.noreply.github.com>
2022-04-22 18:37:48 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -41,7 +41,6 @@ $("#Cat").click(function(){
 
 $("#Reload").click(function(){
   $("#Cat").attr("src","images/box.png");
-  animateFloat();
 });
 
 function animateFloat() {
@@ -50,14 +49,12 @@ function animateFloat() {
   }
   else {
     $("#Container").animate({height: '100vh'},500);
-    return;
   }
   if ($('#Cat').attr('src') == 'images/box.png') {
     $("#Container").animate({height: '90vh'},1000,animateFloat);
   }
   else {
     $("#Container").animate({height: '100vh'},500);
-    return;
   }
 }
 
