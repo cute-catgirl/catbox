33ad0504b16cb8c59e4d194bf6080ac66afaed94
cute-catgirl <benpmoon@gmail.com>
2024-04-18 20:47:09 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var catNum = 0;
-var creditsList = ['Photo by <a href="https://unsplash.com/@e_d_g_a_r?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Edgar</a> on <a href="https://unsplash.com/s/photos/cat?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>','Photo by <a href="https://unsplash.com/@remino?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Rémi Rémino</a> on <a href="https://unsplash.com/s/photos/cat?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>','Photo by <a href="https://unsplash.com/@michaelsum1228?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Michael Sum</a> on <a href="https://unsplash.com/s/photos/cat?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>','Photo by <a href="https://unsplash.com/@mimvafa?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Marzie Vafa</a> on <a href="https://unsplash.com/s/photos/cat?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>','Photo by <a href="https://unsplash.com/@milada_vigerova?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Milada Vigerova</a> on <a href="https://unsplash.com/s/photos/cat?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>'];
+var creditsList = ['Photo by <a href="https://unsplash.com/@e_d_g_a_r?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Edgar</a> on <a href="https://unsplash.com/s/photos/cat?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>','Photo by <a href="https://unsplash.com/@remino?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Rémi Rémino</a> on <a href="https://unsplash.com/s/photos/cat?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>','Photo by <a href="https://unsplash.com/@michaelsum1228?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Michael Sum</a> on <a href="https://unsplash.com/s/photos/cat?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>','Photo by <a href="https://unsplash.com/@mimvafa?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Marzie Vafa</a> on <a href="https://unsplash.com/s/photos/cat?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>','Photo by <a href="https://unsplash.com/@milada_vigerova?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Milada Vigerova</a> on <a href="https://unsplash.com/s/photos/cat?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>', 'Photo by synsao (@echoscapes) on Discord'];
 var catsFound = [];
 var boxesOpened = 0;
 if (localStorage.getItem("catsSave") != null) {
@@ -22,7 +22,7 @@ $.modal.defaults = {
 };
 
 $("#Cat").click(function(){
-  catNum = getRandomInt(1, 5);
+  catNum = getRandomInt(1, 6);
   if ($(this).attr('src') == 'images/box.png') {
     boxesOpened++;
     localStorage.setItem("boxSave", JSON.stringify(boxesOpened));
